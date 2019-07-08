20d8c8789e073d3bd16033859d8c960031b94dc8
goyalanu <33743053+goyalanu@users.noreply.github.com>
2019-07-08 23:16:57 +0530

    Create import_dataSet.py

---
+++ Regression/Decision_tree/import_dataSet.py
@@ -0,0 +1,11 @@
+# Decision Tree Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
