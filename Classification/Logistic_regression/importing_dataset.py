d18bcae9329e448058f92a27b7143cb057f46d74
goyalanu <33743053+goyalanu@users.noreply.github.com>
2019-07-09 20:13:06 +0530

    Create importing_dataset.py

---
+++ Classification/Logistic_regression/importing_dataset.py
@@ -0,0 +1,11 @@
+# Logistic Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
