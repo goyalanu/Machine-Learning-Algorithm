32fd7a7263d4d75f9ccff7ca849c5e6478a68d5a
goyalanu <33743053+goyalanu@users.noreply.github.com>
2019-07-09 20:43:02 +0530

    Create spliting_data_training_test.py

---
+++ Classification/K_Nearest_Neighbors/spliting_data_training_test.py
@@ -0,0 +1,15 @@
+# K-Nearest Neighbors (K-NN)
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
