e651355d7c7968b5374b1e32520ded8ee32a6f81
goyalanu <33743053+goyalanu@users.noreply.github.com>
2019-07-15 11:31:41 +0530

    Create data_preprocessing.py

---
+++ Association_Rule_learning/eclat/data_preprocessing.py
@@ -0,0 +1,9 @@
+# Eclat
+
+# Data Preprocessing
+# install.packages('arules')
+library(arules)
+dataset = read.csv('Market_Basket_Optimisation.csv')
+dataset = read.transactions('Market_Basket_Optimisation.csv', sep = ',', rm.duplicates = TRUE)
+summary(dataset)
+itemFrequencyPlot(dataset, topN = 10)
