b0b6e4303cb48acd7950e0ea8631087c9dd03761
goyalanu <33743053+goyalanu@users.noreply.github.com>
2019-07-15 11:37:55 +0530

    Create importing_dataset.py

---
+++ Rainforcement_learning/Upper_confidence_bound/importing_dataset.py
@@ -0,0 +1,9 @@
+# Upper Confidence Bound
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
