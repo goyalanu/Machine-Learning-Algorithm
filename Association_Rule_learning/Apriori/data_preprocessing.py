05d5f4f2ac41a5be4440c37ca028e80381a9a1f7
goyalanu <33743053+goyalanu@users.noreply.github.com>
2019-07-15 11:22:17 +0530

    Create data_preprocessing.py

---
+++ Association_Rule_learning/Apriori/data_preprocessing.py
@@ -0,0 +1,12 @@
+# Apriori
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Data Preprocessing
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+transactions = []
+for i in range(0, 7501):
+    transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
