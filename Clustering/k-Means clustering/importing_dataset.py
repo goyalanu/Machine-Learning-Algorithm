f7146b0823a46b937b9822dac025b2d36e5a2b9b
goyalanu <33743053+goyalanu@users.noreply.github.com>
2019-07-15 10:56:28 +0530

    Create importing_dataset.py

---
+++ Clustering/k-Means clustering/importing_dataset.py
@@ -0,0 +1,11 @@
+# K-Means Clustering
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:, [3, 4]].values
+# y = dataset.iloc[:, 3].values
