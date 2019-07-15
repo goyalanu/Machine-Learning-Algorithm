24de23a2d4029094d3f69a83835300d2fbd7100b
goyalanu <33743053+goyalanu@users.noreply.github.com>
2019-07-15 12:04:26 +0530

    Create importing_dataset.py

---
+++ Rainforcement_learning/thompson_sampling/importing_dataset.py
@@ -0,0 +1,9 @@
+# Thompson Sampling
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
